bed8cd70ab236cff0a48211301d5901ff48f0796
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2023-09-15 18:35:00 +0700

    setting model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'activity_name',
+        'task_name',
+        'location',
+        'fiscal_year', // tahun
+        'spk_number',
+        'spk_date',
+        'contract_value',
+        'supervising_consultant_id', // id konsultan pengawas
+        'partner_id', // id rekanan
+        'site_supervisor_id_1', // id pengawas lapangan 1
+        'site_supervisor_id_2', // id pengawas lapangan 2
+        'acting_commitment_marker_id', // id ppk
+        'status',
+    ];
 }
