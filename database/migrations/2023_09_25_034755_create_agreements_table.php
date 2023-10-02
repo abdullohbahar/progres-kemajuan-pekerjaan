42af10309f0a2e6ab67f915056b80025b8795cf7
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2023-10-02 11:54:36 +0700

    add add task report when spk date is started

--- database/migrations/2023_09_25_034755_create_agreements_table.php
+++ database/migrations/2023_09_25_034755_create_agreements_table.php
@@ -17,9 +17,12 @@ return new class extends Migration
             $table->id();
             $table->foreignUuid('user_id')->nullable()->constrained('users')->nullOnDelete(); // id user
             $table->foreignUuid('task_report_id')->nullable()->constrained('task_reports')->nullOnDelete(); // id detail pekerjaan
-            $table->string('week');
+            $table->foreignUuid('kind_of_work_detail_id')->nullable()->constrained('kind_of_work_details')->nullOnDelete(); // id detail pekerjaan
             $table->string('role');
-            $table->enum('status', ['Setujui', 'Tolak']);
+            $table->string('week');
+            $table->string('date');
+            $table->string('progress');
+            $table->enum('status', ['Disetujui Rekanan', 'Ditolak Rekanan', 'Disetujui Pengawas Lapangan 1', 'Ditolak Pengawas Lapangan 1', 'Disetujui Pengawas Lapangan 2']);
             $table->timestamps();
         });
     }
