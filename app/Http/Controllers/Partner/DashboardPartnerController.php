93c37cee99a97cfb58bab5a3af6f8dc6605c08ad
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2023-10-11 10:27:12 +0700

    show task in dashboard partner

--- app/Http/Controllers/Partner/DashboardPartnerController.php
+++ app/Http/Controllers/Partner/DashboardPartnerController.php
@@ -2,15 +2,41 @@
 
 namespace App\Http\Controllers\Partner;
 
-use App\Http\Controllers\Controller;
+use App\Models\Partner;
+use App\Models\TaskReport;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardPartnerController extends Controller
 {
     public function index()
     {
+        $dateNow = now();
+
+        $partnerID = Partner::where('user_id', Auth::user()->id)->first();
+
+        $activeWorks = TaskReport::where('spk_date', '<=', $dateNow)
+            ->where('status', 'not like', '%SP%')
+            ->where('status', 'not like', '%SC%')
+            ->where('partner_id', $partnerID->id)
+            ->get();
+
+        $inactiveWorks = TaskReport::where('spk_date', '>=', $dateNow)
+            ->where('status', 'not like', '%SP%')
+            ->where('status', 'not like', '%SC%')
+            ->where('partner_id', $partnerID->id)
+            ->get();
+
+        $spWorks = TaskReport::where('status', 'like', '%SP%')
+            ->where('partner_id', $partnerID->id)
+            ->get();
+
         $data = [
-            'active' => 'dashboard'
+            'active' => 'dashboard',
+            'activeWorks' => $activeWorks,
+            'inactiveWorks' => $inactiveWorks,
+            'spWorks' => $spWorks,
         ];
 
         return view('partner.dashboard.index', $data);
